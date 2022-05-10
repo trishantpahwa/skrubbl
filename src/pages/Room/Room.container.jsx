961c30333c30d54f928c85084cfb8aeea0d0644d
Trishant Pahwa <trishantpahwa@gmail.com>
2022-05-10 19:23:43 +0530

    Removed log(debug) statements.

--- src/pages/Room/Room.container.jsx
+++ src/pages/Room/Room.container.jsx
@@ -38,7 +38,6 @@ export default function RoomContainer(props) {
     }
 
     const connectToPeerServer = () => {
-        console.log(import.meta.env)
         setPeer(new Peer(`${roomID}-${id}`, {
             host: import.meta.env.VITE_PEER_SERVER_IP,
             port: '',
