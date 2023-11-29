45a30341ce3d3e9f980fb67e4b257548f4b477b3
Trishant Pahwa <trishantpahwa@gmail.com>
2023-11-30 00:01:51 +0530

    Fixed workflows, updated .env settings.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { DrawingBoard, ViewBoard } from './components';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import './App.css'
 
 import {
--- src/helpers/firebase.config.js
+++ src/helpers/firebase.config.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+    apiKey: import.meta.env._FIREBASE_API_KEY,
+    authDomain: import.meta.env._FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env._FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env._FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env._FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env._FIREBASE_APP_ID,
+    measurementId: import.meta.env._FIREBASE_MEASUREMENT_ID,
 };
 
 export default firebaseConfig;
